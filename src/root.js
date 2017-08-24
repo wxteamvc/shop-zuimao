9c0e2fbc8a5d5b21a163c5575659a1648696c64e
Your Name <you@example.com>
2017-08-24 09:22:46 +0800

    框架搭建

---
+++ src/root.js
@@ -0,0 +1,23 @@
+/**
+ * APP入口文件
+ * 使用redux的Provider进行包裹
+ */
+"use strict";
+
+import React, { Component } from 'react';
+import {
+  View
+} from 'react-native';
+import { Provider } from 'react-redux';
+
+
+export default class Root extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
