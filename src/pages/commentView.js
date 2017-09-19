acacc6ecd45e71a9587bc7fa5cc54cf022fedbc7
wxteamvc <wxteamvc@163.com>
2017-09-19 17:32:55 +0800

    yangjie

--- src/pages/commentView.js
+++ src/pages/commentView.js
@@ -1,23 +1,39 @@
 /**
- * 订单详情
+ * 评价
  */
 
 "use strict";
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, StatusBar, TouchableOpacity } from 'react-native';
-import { ScreenWidth, ScreenHeight,COMMENT_URL } from '../common/global';
+import { StyleSheet, Text, View, ScrollView, Image, StatusBar, TouchableOpacity, TextInput, Platform } from 'react-native';
+import { ScreenWidth, ScreenHeight, COMMENT_URL } from '../common/global';
 import Util from '../common/util';
 import Toast from 'react-native-root-toast';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import { orderList } from '../actions/orderListAction';
+import ImagePicker from 'react-native-image-picker';
+var options = {
+    title: '请选择图片',
+    cancelButtonTitle: '取消',
+    takePhotoButtonTitle: '拍照片',
+    chooseFromLibraryButtonTitle: '我的相册',
+    quality: 0.75,
+    allowsEditing: true,
+    noData: false,
+    storageOptions: {
+        skipBackup: true,
+        path: 'images'
+    }
+}
 
 class Comment extends Component {
 
     constructor(...props) {
         super(...props);
         this.state = {
-            commentData:''
+            commentData: '',
+            avatarSource: [],
+            comments:{},
         }
     }
 
@@ -46,9 +62,9 @@ class Comment extends Component {
 
     render() {
         if (this.state.commentData != '') {
-            // console.log('====================================');
-            // console.log(this.state.commentData);
-            // console.log('====================================');
+            console.log('====================================');
+            console.log(this.state.comments);
+            console.log('====================================');
             return (
                 <View style={{ flex: 1 }}>
                     <StatusBar
@@ -58,6 +74,11 @@ class Comment extends Component {
                     <ScrollView>
                         {this.renderGoods()}
                     </ScrollView>
+                    <View style={s.btnBox}>
+                        <TouchableOpacity>
+                            <Text style={s.btn}>确认评价</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             )
         } else {
@@ -68,13 +89,36 @@ class Comment extends Component {
 
     renderGoods() {
         let goods = this.state.commentData.goods;
-        let goodsArr=[];
+        let goodsArr = [];
         for (let i = 0; i < goods.length; i++) {
             goodsArr.push(
-                <View key={i}>
+                <View key={i} style={{marginTop:10}}>
                     <View style={s.top}>
-                        <Image source={{uri:goods[i].thumb}} style={{width:50,height:50,marginRight:20}}/>
+                        <Image source={{ uri: goods[i].thumb }} style={{ width: 50, height: 50, marginRight: 20 }} />
                         {this.renderStar(goods[i].id)}
+                    </View>
+                    <View style={s.centet}>
+                        <TextInput
+                            style={{ textAlignVertical: 'top' }}
+                            numberOfLines={5}
+                            onChangeText={(text) => {
+                                this.setState({
+                                    comment:Object.assign({},this.state.comments[i],{content:text})
+                                })
+                            }}
+                            multiline={true}
+                            placeholder='宝贝满足您的期待吗？说说它的有点和美中不足的地方吧'
+                            underlineColorAndroid="transparent"
+                        />
+                    </View>
+                    <View style={s.pic}>
+                        <TouchableOpacity style={s.addPic} onPress={() => {
+                            this._imagePicker(i)
+                        }}>
+                            <Icon name="camera" size={30} />
+                            <Text style={{ fontSize: 11 }}>添加图片</Text>
+                        </TouchableOpacity>
+                        {this.renderImg()}
                     </View>
                 </View>
             )
@@ -82,13 +126,59 @@ class Comment extends Component {
         return goodsArr;
     }
 
-    renderStar(gid){
-        let starArr=[];
+    renderImg() {
+        let imgs = this.state.avatarSource;
+        let imgArr = [];
+        if (imgs.length > 0) {
+            for (let i = 0; i < imgs.length; i++) {
+                imgArr.push(
+                    <View key={i}>
+                        <Image source={{ uri: imgs[i].uri }} style={s.img} />
+                        <TouchableOpacity style={{ position: 'absolute', top: 0, right: 0}} onPress={()=>this._delImg(i)}>
+                            <Icon name="times-circle"size={20} color={'red'} style={{backgroundColor:'#fff',borderRadius:20}}/>
+                        </TouchableOpacity>
+                    </View>
+                )
+
+            }
+        }
+        return imgArr;
+    }
+
+    _delImg(i){
+        let imgs = this.state.avatarSource;
+        imgs.splice(i,1);
+        this.setState({
+            avatarSource:imgs
+        })
+    }
+
+    _imagePicker = (i) => {
+        ImagePicker.showImagePicker(options, (res) => {
+            if (res.didCancel) {  // 返回
+                return
+            } else {
+                let source;  // 保存选中的图片
+                source = { uri: 'data:image/jpeg;base64,' + res.data };
+                if (Platform.OS === 'android') {
+                    source = { uri: res.uri };
+                } else {
+                    source = { uri: res.uri.replace('file://', '') };
+                }
+                this.setState({
+                    avatarSource: [...this.state.avatarSource, source]
+                });
+            }
+        })
+    }
+
+    renderStar(gid) {
+        let starArr = [];
         for (var i = 0; i < 5; i++) {
             starArr.push(
-                <TouchableOpacity key={i}><Icon name="star-o" size={25} style={s.star}/></TouchableOpacity>
+                <TouchableOpacity key={i}><Icon name="star-o" size={25} style={s.star} /></TouchableOpacity>
             )
-            
+
         }
         return (
             <View style={s.starBox}>
@@ -100,21 +190,49 @@ class Comment extends Component {
 }
 
 const s = StyleSheet.create({
-    star:{
-        marginLeft:10,
-        marginRight:10
+    img: {
+        height: 70, width: 70, margin: 5,
     },
-    starBox:{
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center'
+    addPic: {
+        padding: 15, borderWidth: 1, borderColor: '#ddd', width: 80, alignItems: 'center'
     },
-    top:{
-        flexDirection:'row',
-        padding:10,
-        backgroundColor:'#fff',
-        borderBottomWidth:1,
-        borderColor:'#ddd'
+    btn: {
+        textAlign: 'center',
+        padding: 10,
+        backgroundColor: 'red',
+        color: '#fff',
+        borderRadius: 5,
+    },
+    btnBox: {
+        padding: 10
+    },
+    pic: {
+        flexDirection: 'row',
+        paddingLeft: 15,
+        paddingTop: 10,
+        paddingBottom: 15,
+        backgroundColor: '#fff',
+        flexWrap: 'wrap',
+    },
+    centet: {
+        padding: 10,
+        backgroundColor: '#fff'
+    },
+    star: {
+        marginLeft: 10,
+        marginRight: 10
+    },
+    starBox: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    top: {
+        flexDirection: 'row',
+        padding: 10,
+        backgroundColor: '#fff',
+        borderBottomWidth: 1,
+        borderColor: '#ddd'
     }
 })
 
