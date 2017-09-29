ea9b6f40a6e25b3d7940ed84e400780f15b6417b
wxteamvc <wxteamvc@163.com>
2017-09-29 16:41:16 +0800

    qy

--- src/launch.js
+++ src/launch.js
@@ -3,13 +3,14 @@
  */
 
 import React, { Component } from 'react';
-import { View,NetInfo } from 'react-native';
+import { View, NetInfo, Image } from 'react-native';
 
 var Dimensions = require('Dimensions');
 var { width, height } = Dimensions.get('window');
 import Loading from './component/loading';
 import { connect } from 'react-redux';
 import { init } from './actions/initAction';
+import { ScreenWidth, ScreenHeight } from './common/global';
 import App from './app';
 
 //全局联网状态
@@ -19,12 +20,18 @@ class Launch extends Component {
 
   constructor(...props) {
     super(...props);
+    this.state = {
+      time: 3
+    }
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Loading />
+        <Image
+          resizeMode={'stretch'}
+          source={require('./assets/images/indexAd.jpg')}
+          style={{ width: ScreenWidth, height: ScreenHeight }} />
       </View>
     );
   }
@@ -43,23 +50,27 @@ class Launch extends Component {
   }
 
   componentDidMount() {
-    this.timer = setTimeout(() => {
-      this.props.navigator.replace({
-        title: '首页',
-        component: App,
-        //    passProps:{'id':1}传值
-      });
-    }, 1000)
     this.props.dispatch(init());
+    this.timer = setInterval(() => {
+      this.setState({ time: this.state.time > 0 ? --this.state.time : 0 })
+      if (this.state.time == 0 && this.props.homeData.status == 'success') {
+        this.props.navigator.replace({
+          title: '首页',
+          component: App,
+          //    passProps:{'id':1}传值
+        });
+      }
+    }, 1000)
+
   }
   componentWillUnmount() {
-    this.timer && clearTimeout(this.timer);
+    this.timer && clearInterval(this.timer);
   }
 }
 
 function mapStateToProps(state) {
   return {
-      // homeData: state.initReducer,
+    homeData: state.initReducer,
   }
 }
 
